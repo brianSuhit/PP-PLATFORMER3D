239f8b01334c128753e8ca0edd3f7e9605fb7225
Brian Andres Suhit <briansuhit000@gmail.com>
2024-03-06 21:11:29 -0300

    Refactoring movement and add character rotation. Init character jump logic

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private float speed = 1f;
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+
     private Vector3 _direction;
 
-    private void Awake()
+    private void Reset()
     {
         characterController = GetComponent<CharacterController>();
     }
+
     private void Update()
     {
-        characterController.Move(new Vector3(_direction.x, 0, _direction.y) * speed * Time.deltaTime);
+
+        Vector3 movementDirection = new Vector3(_direction.x, 0f, _direction.y);
+        if (movementDirection.sqrMagnitude > Mathf.Epsilon)
+        {
+            movementDirection.Normalize();
+        }
+
+        if (movementDirection.sqrMagnitude == 0) return;
+
+
+        float targetAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+        //characterController.Move(new Vector3(_direction.x, 0, _direction.y) * speed * Time.deltaTime);
+
     }
 
     public void SetDirection(Vector2 direction)
--- Assets/Scripts/Player/PlayerInputReaderController.cs
+++ Assets/Scripts/Player/PlayerInputReaderController.cs
@@ -4,12 +4,24 @@ using UnityEngine.InputSystem;
 public class PlayerInputReaderController : MonoBehaviour
 {
     [SerializeField] private CharacterMovement characterMovement;
+    [SerializeField] private CharacterJump characterJump;
 
     public void SetMovementValue(InputAction.CallbackContext inputContext)
     {
         Vector2 inputValue = inputContext.ReadValue<Vector2>();
         characterMovement.SetDirection(inputValue);
+    }
 
-        Debug.Log($"{gameObject.name}: Event risen. Value: {inputValue}");
+    public void RequestJump(InputAction.CallbackContext inputContext)
+    {
+        if (inputContext.started)
+        {
+            if (characterJump == null)
+            {
+                Debug.LogError($"{name} character jump is null");
+                return;
+            }
+            //characterJump.Jump();
+        }
     }
 }
